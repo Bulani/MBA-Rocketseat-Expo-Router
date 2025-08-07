3437a28e97fa5e864e223a332e935ccdb598b6c0
Bulani <jao_1999@hotmail.com>
2025-08-07 00:20:51 -0300

    new: conceitos da aula

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,27 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet, TouchableOpacity,Text } from 'react-native'
+import { router } from 'expo-router'
 
 export default function Index(){
+  function signUp(){
+    router.navigate("/sign-up")
+  }
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Entrar</Text>
+      <TouchableOpacity style={styles.button} onPress={signUp}>
+        <Text style={styles.label}>Criar Conta</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  title: { fontSize: 22, fontWeight: "bold" }
+  container: { flex: 1, justifyContent: "center", alignItems: "center", gap: 32 },
+  label: { fontSize: 16, fontWeight: "bold", color: "#FFF" },
+  button: {
+    backgroundColor: "#000",
+    paddingHorizontal: 32,
+    paddingVertical: 10,
+    borderRadius: 10,
+  }
 })
---
+++ src/app/sign-up.tsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import { router } from 'expo-router'
+
+export default function SignUp(){
+  function back(){
+    if(!router.canGoBack)
+      return Alert.alert('Não é possível voltar!')
+
+    router.back()
+  }
+  
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={back}>
+        <Text style={styles.back}>Voltar</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  back: { fontSize: 16, fontWeight: "bold" },
+  title: { fontSize: 22 }
+})
